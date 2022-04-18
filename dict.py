2044fce1c5d8c173827561d1ac89295af0a00e8b
buladou <lalasahara@outlook.com>
2022-04-18 19:54:11 +0900

    fourth python file

---
+++ dict.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+
+
+#scores = {'tom':98,'jack':100,'alice':60}
+#print(scores)
+#print(type(scores))
+#print(scores['jack'])
+#print(scores.get('alice'))
+#scores['lucy'] = 99
+#scores['tom'] = 100
+#print(scores.pop('tom'))
+#print(scores)
+
+#print ('alice' in scores) #True
+
+#print(scores.keys()) # dict_keys(['tom', 'jack', 'alice'])
+#print(scores.values()) # dict_values([98, 100, 60])
+#print(scores.items()) # dict_items([('tom', 98), ('jack', 100), ('alice', 60)])
+
+'''for k in scores.keys():
+    print(k,scores[k]) '''
+''' for v in scores.values():
+    print(v)
+print('-' * 30) '''
+
+'''
+for item in scores.items():
+    print(item)
+''' 
+#for k,v in scores.items():
+#    print(k,v)
+''' 
+tom 98
+jack 100
+alice 60
+ '''
+
+'''
+names=['alice','tom', 'tom','mile','mike','tom']
+print(names.count('tom')) #3
+'''
+
+'''
+s = {3,12,5,7,34} # this called set
+print(s,type(s)) # {34, 3, 5, 7, 12} <class 'set'>
+''' 
+
+'''
+s={3,5,6,3,2,3,7,9,0}
+print(s) #{0, 2, 3, 5, 6, 7, 9}
+'''
+
+
+li=[2,4,8,9,2,4,7,7,7,9,56]
+s=set(li)
+#print(s,type(s)) #{2, 4, 7, 8, 9, 56} <class 'set'>
+#s.add(666)
+#s.add(2)
+#s.remove(8)
+
+#print(s)
+'''
+for i in s:
+    print(i,end=',')
+print(s)
+'''
+# 2,4,7,8,9,56,{2, 4, 7, 8, 9, 56}
+
